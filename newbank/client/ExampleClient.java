dfdd960184ff734b48a3f9605e0ae1619483a844
Hasse Hämäläinen <hasse.hamalainen@gmail.com>
2022-11-13 13:52:55 +0100

    Revert and create a new branch
    This reverts commit 541c2a27616dc9d0e878a80ae6937158d0a9d746.

--- newbank/client/ExampleClient.java
+++ newbank/client/ExampleClient.java
@@ -1,4 +1,5 @@
 package newbank.client;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,47 +9,44 @@ import java.net.UnknownHostException;
 
 public class ExampleClient extends Thread{
 	
-	private final Socket server;
-	private final PrintWriter bankServerOut;
-	private final BufferedReader userInput;
-
+	private Socket server;
+	private PrintWriter bankServerOut;	
+	private BufferedReader userInput;
+	private Thread bankServerResponceThread;
+	
 	public ExampleClient(String ip, int port) throws UnknownHostException, IOException {
 		server = new Socket(ip,port);
 		userInput = new BufferedReader(new InputStreamReader(System.in)); 
-		bankServerOut = new PrintWriter(server.getOutputStream(), true);
-
-		Thread bankServerResponseThread = new Thread() {
-			private final BufferedReader bankServerIn = new BufferedReader(new InputStreamReader(server.getInputStream()));
-
+		bankServerOut = new PrintWriter(server.getOutputStream(), true); 
+		
+		bankServerResponceThread = new Thread() {
+			private BufferedReader bankServerIn = new BufferedReader(new InputStreamReader(server.getInputStream())); 
 			public void run() {
-				while (true) {
-					try {
-						{
-							String response = bankServerIn.readLine();
-							System.out.println(response);
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-						return;
+				try {
+					while(true) {
+						String responce = bankServerIn.readLine();
+						System.out.println(responce);
 					}
+				} catch (IOException e) {
+					e.printStackTrace();
+					return;
 				}
 			}
 		};
-		bankServerResponseThread.start();
+		bankServerResponceThread.start();
 	}
 	
 	
 	public void run() {
-		while (true) {
+		while(true) {
 			try {
-				{
+				while(true) {
 					String command = userInput.readLine();
 					bankServerOut.println(command);
 				}				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-				return;
 			}
 		}
 	}
