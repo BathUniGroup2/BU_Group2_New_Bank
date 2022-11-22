644b06b42604aa9a74110f1ce948e08a0d098373
Eoin O'Toole <eoinjamesotoole@gmail.com>
2022-11-22 21:31:05 +0000

    Revert "Made the code to work"

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -32,7 +32,7 @@ public class NewBank {
 	}
 
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if (customers.containsKey(userName)) {
+		if(customers.containsKey(userName)) {
 			return new CustomerID(userName);
 		}
 		return null;
@@ -40,35 +40,19 @@ public class NewBank {
 
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request, String[] args) {
-		if (customers.containsKey(customer.key())) {
-			return switch (request) {
-				case "SHOWMYACCOUNTS" -> showMyAccounts(customer);
-				case "NEWACCOUNT" -> newAccount(customer, args);
-				case "PAY" -> pay(customer, args);
-        case "MOVE" : return Move(customer, args);
-				default -> "FAIL";
-			};
+		if(customers.containsKey(customer.getKey())) {
+			switch(request) {
+				case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
+				case "NEWACCOUNT" : return newAccount(customer, args);
+				case "MOVE" : return Move(customer, args);
+				default : return "FAIL";
+			}
 		}
 		return "FAIL";
 	}
 
-	/**
-	 * This method is used to show the customer's accounts
-	 *
-	 * @param customer CustomerID of the customer
-	 * @return the names of the customer's accounts and their balances
-	 */
 	private String showMyAccounts(CustomerID customer) {
-		Customer currentCustomer = customers.get(customer.key());
-		ArrayList<Account> currentAccounts = currentCustomer.getAccounts();
-		if (currentAccounts.size() < 1) return null;
-		ArrayList<String> accountList = new ArrayList<>();
-		for (Account currentAccount : currentAccounts) {
-			{
-				accountList.add(currentAccount.getAccountType().toString() + ": " + currentAccount.getBalance());
-			}
-		}
-		return String.join("\n", accountList);
+		return (customers.get(customer.getKey())).accountsToString();
 	}
 
 	private String newAccount(CustomerID customer, String[] accountTypes) {
@@ -90,72 +74,12 @@ public class NewBank {
 			Account newAccount = new Account(Account.stringToAccountType(accountType), 0.0);
 			currentCustomer.addAccount(newAccount);
 			return "SUCCESS";
-		} catch (Exception e) {
+		} catch(Exception e) {
 			return "FAIL";
 		}
 	}
 
-	/**
-	 * Transfer money from one account to another
-	 * @param customer CustomerID of the customer making the payment
-	 * @param args The arguments for the transfer (customerAccountType, payee, payeeAccountType, amount)
-	 * @return String of "SUCCESS" or "FAIL"
-	 */
-	private String pay(CustomerID customer, String[] args) {
-		String customerAccountType = args[0];
-		String payee = args[1];
-		String payeeAccountType = args[2];
-		String amount = args[3];
-
-		// Fail if no payee, account or amount argument was given
-		if (payee.length() < 1 || customerAccountType.length() < 1 ||
-				amount.length() < 1 || payeeAccountType.length() < 1) return "FAIL";
-
-		Customer currentCustomer = customers.get(customer.key());
-		ArrayList<Account> currentAccounts = currentCustomer.getAccounts();
-
-		Account currentAccount = null;
-		// Fail if customer has no accounts
-		if (currentAccounts.size() < 1) return "FAIL";
-		// Fail if customer has no account of corresponding to customerAccountType
-		for (Account _currentAccount : currentAccounts) {
-			if (_currentAccount.getAccountType().toString().equals(customerAccountType)) {
-				// Fail if customer has no sufficient funds in the selected account
-				if (_currentAccount.getBalance() < Double.parseDouble(amount)) {
-					return "FAIL";
-				} else {
-					currentAccount = _currentAccount;
-				}
-			}
-		}
-		if (currentAccount == null) return "FAIL";
-
-
-		Account payeeAccount = null;
-		// If the payee is a customer of the bank, transfer the money to their account
-		if (customers.containsKey(payee)) {
-			Customer payeeCustomer = customers.get(payee);
-			ArrayList<Account> payeeAccounts = payeeCustomer.getAccounts();
-			// Fail if payee has no accounts
-			if (payeeAccounts.size() < 1) return "FAIL";
-			// Fail if payee has no account of corresponding to payeeAccountType
-			for (Account _payeeAccount : payeeAccounts) {
-				if (_payeeAccount.getAccountType().toString().equals(payeeAccountType)) {
-					payeeAccount = _payeeAccount;
-					currentAccount.setBalance(currentAccount.getBalance() - Double.parseDouble(amount));
-					payeeAccount.setBalance(payeeAccount.getBalance() + Double.parseDouble(amount));
-				}
-			}
-			if (payeeAccount == null) return "FAIL";
-			// no functionality to transfer money outside the bank requested
-		} else return "FAIL";
-
-		// succeed if no condition above fails
-		return "SUCCESS";
-	}
-}
-
-  private String Move(CustomerID customer, String[] args) {
+	private String Move(CustomerID customer, String[] args) {
 		String from = args[0];
 		String to = args[1];
 		String amount = args[2];
