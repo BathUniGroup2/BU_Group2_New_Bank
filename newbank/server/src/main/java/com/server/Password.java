774c51482c6d13f466dd5add644c5fce2e2a77f5
TR4SPY <33313091+TR4SPY@users.noreply.github.com>
2022-11-27 21:40:20 +0000

    Created Password.java
    Class Password containing functionality to store & verify user passwords.

---
+++ newbank/server/src/main/java/com/server/Password.java
@@ -0,0 +1,13 @@
+package com.server;
+
+public class Password {
+    private String password;
+
+    public Password(String password) {
+        this.password = password;
+    }
+
+    public boolean verifyPassword(String password) {
+        return this.password.equals(password);
+    }
+}
